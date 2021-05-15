9a927e5752810f68f761fcb90c47d85c056e8312
shwetap29 <shwetaprasad48@gmail.com>
2021-05-15 16:24:57 +0530

    flipped coin multiple times & shown head & tails occurance in FlipCoinSimulator.sh

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -3,12 +3,36 @@
 printf "Welcome to flip coin simulation \n"
 #generating RANDOM VALUE
 result=$((RANDOM%2+1))
-isHead=1
-#condition Is true then Head else Tail
-if(($isHead == $result))
-then
-      echo "Head"
-else
-      echo "Tail"
-fi
-
+#constant variable 
+COUNT_HEAD=0
+COUNT_TAIL=0
+read -p "Enter how many times to flip coin: " userInput
+TEMP=$userInput
+#INITIALIZE DICTIONARY
+declare -A Flippingcoin
+#using loop to flip coin multiple times
+for(( i=1; i<=$userInput; i++ ))
+       do
+          #GENERATING RANDOM VALUE
+          result=$((RANDOM%2+1))
+          case $result in
+                1)
+                   COUNT_HEAD=$(($COUNT_HEAD+1))
+                  FlippingCoin["H"]="$COUNT_HEAD"
+                   ;;
+                 2)
+                   COUNT_TAIL=$(($COUNT_TAIL+1))
+                   FlippingCoin["T"]="$COUNT_TAIL"
+				;;
+		esac
+	done
+headPercentage=`echo "scale=3; $COUNT_HEAD / $TEMP * 100 "  #|bc -l`
+tailPercentage=`echo "scale=3; $COUNT_TAIL / $TEMP * 100 "  #|bc -l`
+#PRINTING KEY AND VALUE OF COIN
+for count in ${!FlippingCoin[@]}
+	do
+		echo "$count : ${FlippingCoin[$count]}"
+	done
+#PRINTING PERcENTAGE OF FLIPING COIN
+echo "Head percentage : $headPercentage "
+echo "Tail percentage : $tailPercentage "
